995409833fbe278e162b6d924890fba6844750b1
medvedevsc2 <m.v.medvedev2006@gmail.com>
2025-02-23 15:09:08 +0300

    first dap

---
+++ src/test/java/com/example/medvedev/hop/Medvedev$hopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medvedev.hop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medvedev$hopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
